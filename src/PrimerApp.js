5ac8fdb15cb8b2952ad25ff5f43fcc2b3bbe6f2f
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-07-05 06:06:44 -0400

    Fix return component elements on html

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -6,8 +6,20 @@ import React from 'react'
 
 
 const PrimeraApp = () => {
-    return <h1>Hola Mundo</h1>;
-}
+    // return <h1>Hola Mundo</h1>;
+    //para el return el js debe retornar un objeto a la vez, no se pueden regresar estas dos cosas
+    //return 
+    // <h1>Hola Mundo</h1>
+    // <p>Mi Primera App</p>;
+    //con cambio de linea no funciona  - Si presiono enter, ya no funciona
+    //PAra arreglaro se usan parenteisis ( ) y ponerlo en elemento root ya sea un div o cualquier otro
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+            <p>Mi Primera App</p>
+        </div>
+        );
+}   
 
 //Esperamos que devuelva HTML 
 export default PrimeraApp;
