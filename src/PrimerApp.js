67645b14f6a43008f661c743ecf7a008a0f6e6f5
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-07-05 05:45:58 -0400

    Added css style and imported file

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -1,7 +1,9 @@
 // SFC ( state Functional Component ) -> HOOK
 //Vamos a trabajr con Functional Component
 //HAy que importar react para que funcione el import del component en el index.js
-import React from 'react';
+//short cut 'imr' and add  import React from 'react' on visual studio code 
+import React from 'react'
+
 
 const PrimeraApp = () => {
     return <h1>Hola Mundo</h1>;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 //Importamos el Componente o funcitonal Component, por que regresa HTML 
 import PrimeraApp from './PrimerApp';
 
+//importacion del Css
+import './index.css'; // es una forma de trabajar css , hay otras 
+
 // const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot = document.querySelector('#app');
