3e046054e01643e4f7fe1f52514f1ba8b5eff0d1
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-07-08 15:11:25 -0400

    Primer Event llamando una function handleAdd

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+// Como manejar eventos click
+//https://es.reactjs.org/docs/events.html 
 const CounterApp = ({value}) => {
+
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
     return (
         <>
             <h1>Counter App</h1>
             <h2>{value}</h2>
+            
+            <button onClick={ handleAdd }> +1 </button>
         </>
     );
 }
