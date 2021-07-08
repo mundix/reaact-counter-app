ebbfe781be7c8be758b2237d337a3d7f23ac5db5
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-07-08 15:28:56 -0400

    Usando Hooks con useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,25 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react'; //cuando algo tenga un 'use' quiere decir que es un hook
 import PropTypes from 'prop-types';
 // Como manejar eventos click
 //https://es.reactjs.org/docs/events.html 
 const CounterApp = ({value}) => {
 
-    const handleAdd = (e) => {
-        console.log(e);
+    //primer hook , Hook no es mas que una funciton
+    // const state = useState('Goku'); //se hace una desetructuracion 
+    // const [nombre, setNombre] = useState('Goku'); 
+    const [ counter , setCounter ] = useState(0); 
+
+    
+    const handleAdd = () => {
+        // setCounter(counter + 1); 
+        setCounter( (c) =>  c + 1  ); //otra forma
     }
 
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
             
             <button onClick={ handleAdd }> +1 </button>
         </>
