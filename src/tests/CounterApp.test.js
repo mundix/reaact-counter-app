9902093799752b79dd4e907867eb96e0642119f3
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-07-26 00:00:02 -0400

    Simulando un click

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -5,10 +5,16 @@ import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
 
 describe('Archivo de pruebas de <CounterApp/>', () => {
+    //movi este wrapper de la primera prueba afuera ene l describe
+    // let wrapper; //inicializado como undefi
+    let wrapper = shallow(<CounterApp/>); //inicializado como undefi
+ 
+    //Esta funciona se va a ejecutar antes de todas las funciones
+    beforeEach(() =>{
+         wrapper = shallow(<CounterApp/>);
+    });
 
     test('Debe mostar <CounterApp/> correctamente', () => {
-        const value=10;
-        const wrapper = shallow(<CounterApp/>);
         
         expect(wrapper).toMatchSnapshot(); // ? 
     });
@@ -19,7 +25,29 @@ describe('Archivo de pruebas de <CounterApp/>', () => {
         const textParrafo = wrapper.find('h2').text().trim(); //busca un <p> solo me interesa el texto
         // expect(textParrafo).toEqual('<h2>'+value+'</h2>');
         expect(textParrafo).toBe('100');
-    })
+    });
+
+    test('Debe incrementar cone l boton +1', () => {
+        
+        //busca todos los botones y selecciona el primero con el .at(0)
+        // const btn1 = wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('11');
+
+    });
+
+
+    test('Debe decrementar cone l boton -1', () => {
+        
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('9');
+
+    });
+    
     
     
 
